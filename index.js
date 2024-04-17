a9f8d99dfde030ca1ca34105678ad45c3bcd7724
AtulSharma406 <167280800+AtulSharma406@users.noreply.github.com>
2024-04-17 11:31:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+ const login= document.getElementById("login");
+ const loginoptions= document.getElementById("loginoptions");
+ login.addEventListener('click',()=>{
+     loginoptions.style.display="block";
+ });
+ const user=document.getElementById("user");
+  const userProfile=document.getElementById("userprofile");
+    user.addEventListener('click',()=>{
+     userProfile.style.display="block";
+  })
+ 
+ 
+
+ 
+
+
+
+
+
+
+ 
+
+ 
---
+++ t-shirts.js
@@ -0,0 +1,87 @@
+
+    const button = document.getElementById("myButton");
+    const cardItem = document.getElementById("carditem");
+    // Attach a click event listener to the button
+  button.addEventListener("click", function() {
+        // When the button is clicked, trigger the alert
+        alert("item added to cart successfully");
+        cardItem.style.display="block";
+ 
+
+    });
+    
+    function openpopup(){
+button.classList.add("openpopup");
+    }
+   
+    const button1 = document.getElementById("myButton1");
+
+    // Attach a click event listener to the button
+    button1.addEventListener("click", function() {
+        // When the button is clicked, trigger the alert
+        alert("item added to cart successfully");
+
+    });
+    const button2 = document.getElementById("myButton2");
+
+    // Attach a click event listener to the button
+    button2.addEventListener("click", function() {
+        // When the button is clicked, trigger the alert
+        alert("item added to cart successfully");
+
+    });
+    const button3 = document.getElementById("myButton3");
+
+    // Attach a click event listener to the button
+    button3.addEventListener("click", function() {
+        // When the button is clicked, trigger the alert
+        alert("item added to cart successfully");
+
+    });
+    const button4 = document.getElementById("myButton4");
+
+    // Attach a click event listener to the button
+    button4.addEventListener("click", function() {
+        // When the button is clicked, trigger the alert
+        alert("item added to cart successfully");
+
+    });
+    const button5 = document.getElementById("myButton5");
+
+    // Attach a click event listener to the button
+    button5.addEventListener("click", function() {
+        // When the button is clicked, trigger the alert
+        alert("item added to cart successfully");
+
+    });
+    const button6 = document.getElementById("myButton6");
+
+    // Attach a click event listener to the button
+    button6.addEventListener("click", function() {
+        // When the button is clicked, trigger the alert
+        alert("item added to cart successfully");
+
+    });
+    const button7 = document.getElementById("myButton7");
+
+    // Attach a click event listener to the button
+    button7.addEventListener("click", function() {
+        // When the button is clicked, trigger the alert
+        alert("item added to cart successfully");
+
+    });
+    const button8 = document.getElementById("myButton8");
+
+    // Attach a click event listener to the button
+    button8.addEventListener("click", function() {
+        // When the button is clicked, trigger the alert
+        alert("item added to cart successfully");
+
+    });
+   /* let popup=document.getElementById("popup");
+      function openpopup(){
+    popup.classList.add("openpopup");
+      }
+      function closepopup(){
+    popup.classList.remove("openpopup");
+      }*/
